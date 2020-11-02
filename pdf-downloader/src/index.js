448d91f7a0af8b6903d9f3e1c4ab773eb71fb038
Vlad Soroceanu <vlad.soroceanu@gmail.com>
2020-11-02 11:33:32 +0000

    adds axios

---
+++ pdf-downloader/src/index.js
@@ -0,0 +1,34 @@
+import Axios from "axios";
+
+const getPdfFile = () => {
+  return Axios.get("dummy.pdf", {
+    responseType: "arraybuffer",
+    headers: { Accept: "application/pdf" },
+  });
+};
+
+window.onload = async () => {
+  try {
+    const { data } = await getPdfFile();
+    console.log(data);
+    const file = new Blob([data]); // , { type: "application/pdf" }
+    const link = document.querySelector("#download-blob");
+    link.href = window.URL.createObjectURL(file);
+    link.download = "byte-array-pdf";
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+// const xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function () {
+//   if (this.readyState == 4 && this.status == 200) {
+//     const file = new Blob([this.response], { type: 'application/pdf' });
+//     const link = document.querySelector('#download-blob');
+//     link.href = window.URL.createObjectURL(file);
+//     link.download = 'byte-array';
+//   }
+// };
+// xhr.open('GET', 'dummy.pdf');
+// xhr.responseType = 'blob';
+// xhr.send();
